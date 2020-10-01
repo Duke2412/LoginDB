86181ac2c680271cc18fec189046a887f542939a
Duke2412 <66355961+Duke2412@users.noreply.github.com>
2020-10-01 15:30:09 +0700

    Dũng nộp bài

---
+++ src/main/java/com/codegym/repositories/IMyUserRepository.java
@@ -0,0 +1,11 @@
+package com.codegym.repositories;
+
+import com.codegym.models.MyUser;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+
+@Qualifier("myUserRepository")
+public interface IMyUserRepository extends CrudRepository<MyUser, Long> {
+    MyUser findByUsername(String username);
+}
